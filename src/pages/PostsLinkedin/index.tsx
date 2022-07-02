f4b9f104300da976a1a35fb0c10ca18d9d5d6869
Dakson Cruz <daksonc@gmail.com>
2022-07-02 18:57:15 -0300

    refactor: configurando as rotas

--- src/pages/PostsLinkedin/index.tsx
+++ src/pages/PostsLinkedin/index.tsx
@@ -1,7 +1,7 @@
-export function ReposGitHub(){
+export function PostsLinkedin(){
     return(
         <div>
-            <h1>Repos GitHub</h1>
+            <h1>Posts Linkedin</h1>
             <a href="/">Home</a> 
         </div>
     )
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import { ReposGitHub } from './ReposGitHub'
+import { PostsLinkedin } from './PostsLinkedin'
 
 export default function MyRoute(){
     return(
@@ -8,6 +9,7 @@ export default function MyRoute(){
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/repos" element={<ReposGitHub />} />
+                <Route path="/posts" element={<PostsLinkedin />} />
             </Routes>        
         </BrowserRouter>
     )
