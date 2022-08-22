69e40824ef58f953c079adfb9f88b043849a34fd
Dakson Cruz <daksonc@gmail.com>
2022-08-22 00:07:25 -0300

    refactor: deletei a pasta routes

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from '../home'
-import { ReposGitHub } from '../repos'
-import { Article } from '../article'
-import { Header } from '../../components/header';
-import { Footer } from '../../components/footer';
-import { Information } from '../../components/information';
+import Home from './home'
+import { ReposGitHub } from './repos'
+import { Article } from './article'
+import { Header } from '../components/header';
+import { Footer } from '../components/footer';
+import { Information } from '../components/information';
 
 export default function MyRoute(){
     return(
